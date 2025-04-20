c2c5496239acc8a546ab928df3d4167c65f12ec0
Will Schipp <3287360+willschipp@users.noreply.github.com>
2025-04-20 14:44:20 +0000

    WIP: backstage init

---
+++ app/backstage/plugins/ik8s-plugin/src/plugin.test.ts
@@ -0,0 +1,7 @@
+import { ik8SPluginPlugin } from './plugin';
+
+describe('ik8s-plugin', () => {
+  it('should export plugin', () => {
+    expect(ik8SPluginPlugin).toBeDefined();
+  });
+});
---
+++ app/backstage/plugins/ik8s-plugin/src/routes.ts
@@ -0,0 +1,5 @@
+import { createRouteRef } from '@backstage/core-plugin-api';
+
+export const rootRouteRef = createRouteRef({
+  id: 'ik8s-plugin',
+});
